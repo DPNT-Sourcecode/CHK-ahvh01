5c708357b0295590e2b5b1323b0ae246b27aa097
sbx_user1051 <sbx_user1051@169.254.36.26>
2025-01-18 07:42:13 +0000

    Sat Jan 18 07:42:13 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -9,18 +9,43 @@ PRODUCT_PRICES = {
     "D": 15,
     "E": 40,
     "F": 10,
+    # ---
+    "G": 20,
+    "H": 10,
+    "I": 35,
+    "J": 60,
+    "K": 80,
+    "L": 90,
+    "M": 15,
+    "N": 40,
+    "O": 10,
+    "P": 50,
+    "Q": 30,
+    "R": 50,
+    "S": 30,
+    "T": 20,
+    "U": 40,
+    "V": 50,
+    "W": 20,
+    "X": 90,
+    "Y": 10,
+    "Z": 50,
 }
 
 DISCOUNT_OFFERS = {
     # Item: [[Amount, Price], ...]
     "A": [[5, 200], [3, 130]],
     "B": [[2, 45]],
+    "H": [[5, 45], [10, 80]],
+    "K": [[2000, 150]],
+    ""
 }
 
 FREEBIES_OFFERS = {
     # Product: Amount, Freebie_Product, Freebie_Amount
     "E": [2, "B", 1],
     "F": [3, "F", 1],
+    "N": [3, "M", 1] 
 }
 
 
@@ -83,5 +108,6 @@ def checkout(skus):
         total = -1
 
     return total
+
 
 
