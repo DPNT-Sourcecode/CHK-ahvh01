8672633ac9ac3ebce52df74f21c794fd1bfcaf79
sbx_user1051 <sbx_user1051@169.254.149.42>
2025-01-18 05:38:09 +0000

    Sat Jan 18 05:38:09 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,5 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
+    def sum(x:int, y:int) -> int:
+        return x + y
     raise NotImplementedError()
--- tes.py
+++ tes.py
@@ -1,1 +1,4 @@
-print("YOLO")
+def s_sum(x:int, y:int) -> int:
+    return x + y
+
+s_sum(1,1)
