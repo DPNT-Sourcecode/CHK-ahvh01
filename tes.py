8fdef8b37a1269cb6f75a93fadb9caaecd2e53e2
sbx_user1051 <sbx_user1051@169.254.36.26>
2025-01-18 07:46:28 +0000

    Sat Jan 18 07:46:28 UTC 2025

--- tes.py
+++ tes.py
@@ -9,18 +9,47 @@ PRODUCT_PRICES = {
     "D": 15,
     "E": 40,
     "F": 10,
+    # ---
+    "G": 20,
+    "H": 10,
+    "I": 35,
+    "J": 60,
+    "K": 80,
+    "L": 90,
+    "M": 15,
+    "N": 40,
+    "O": 10,
+    "P": 50,
+    "Q": 30,
+    "R": 50,
+    "S": 30,
+    "T": 20,
+    "U": 40,
+    "V": 50,
+    "W": 20,
+    "X": 90,
+    "Y": 10,
+    "Z": 50,
 }
 
 DISCOUNT_OFFERS = {
     # Item: [[Amount, Price], ...]
     "A": [[5, 200], [3, 130]],
     "B": [[2, 45]],
+    "H": [[5, 45], [10, 80]],
+    "K": [[2000, 150]],
+    "P": [[5, 200]],
+    "Q": [[3, 80]],
+    "V": [[2, 90], [3, 130]],
 }
 
 FREEBIES_OFFERS = {
     # Product: Amount, Freebie_Product, Freebie_Amount
     "E": [2, "B", 1],
-    "F": [3, "F", 1],
+    "F": [2, "F", 1],
+    "N": [3, "M", 1],
+    "R": [3, "Q", 1],
+    "U": [3, "U", 1],
 }
 
 
@@ -29,11 +58,14 @@ def process_freebies(cart, total):
         if cart.get(item):
             item_amount_required, freebie_product, freebies_amount = offer
             while True:
+                if item == freebie_product:
+                    item_amount_required += freebies_amount
+
                 if (
                     cart[item] // item_amount_required
                     and cart[freebie_product] // freebies_amount
                 ):
-                    if item == "F":
+                    if item == freebie_product:
                         total += (
                             item_amount_required - freebies_amount
                         ) * PRODUCT_PRICES[item]
@@ -85,7 +117,6 @@ def checkout(skus):
     return total
 
 
-print(checkout("EEB"))
 
 
 
