3c7d7a12f1d2c0fa01e735b106287206c5087619
sbx_user1051 <sbx_user1051@169.254.36.26>
2025-01-18 06:58:54 +0000

    Sat Jan 18 06:58:54 UTC 2025

--- tes.py
+++ tes.py
@@ -1,24 +1,29 @@
 from collections import defaultdict
+PRODUCT_PRICES = {
+    "A": 50,
+    "B": 30,
+    "C": 20,
+    "D": 15,
+    "E": 40,
+}
 
+DISCOUNT_OFFERS = {
+    # Item: [[Amount, Price], ...]
+    "A": [[5, 200], [3, 130]],
+    "B": [[2, 45]],
+}
+
+freebies_offers = {
+    # Product: Amount, Freebie_Product, Freebie_Amount
+    "E": [2, "B", 1],
+}
 
 def checkout(skus):
-    products_prices = {
-        "A": 50,
-        "B": 30,
-        "C": 20,
-        "D": 15,
-        "E": 40,
-    }
-    
-
     cart = defaultdict(lambda: 0)
     total = 0
 
     def process_freebies(cart, total):
-        freebies_offers = {
-            # Product: Amount, Freebie_Product, Freebie_Amount
-            "E": [2, "B", 1],
-        }
+        
     
         for item, offer in freebies_offers.items():
             if cart.get(item):
@@ -33,21 +38,22 @@ def checkout(skus):
         return total, cart
 
     
-    def process_discounts(cart, total):
-        discount_offers = {
-            # Item: [[Amount, New_Total], ...]
-            "A": [[5, 200], [3, 130]],
-            "B": [[2, 45]],
-        }
-        
-        for item, offers in discount_offers.items():
-            if item in discount_offers.keys():
+    def process_discounts(cart, total):        
+        for item, offers in DISCOUNT_OFFERS.items():
+            if item in cart.keys():
                 for item_amount_required, discount_price in offers:
                     while True:
-                        if amount // item_amount_required:
-                            amount -= item_amount_required
+                        if cart[item] // item_amount_required:
+                            cart[item] -= item_amount_required
                             total += discount_price
+                        else:
+                            break
+        return total, cart
                 
+    def process_cart(cart, total):
+        for item, amount in cart.items()
+            total += amount * PRODUCT_PRICES[item]
+        
         
         
         
@@ -56,25 +62,17 @@ def checkout(skus):
         for i in skus:
             cart[i] += 1
         
+            
+        
+
+        for item in items_order:
+            amount = cart[item]
+
         # ! PROCESS FREEBIES
         total, cart = process_freebies(cart, total)
         # ! PROCESS DISCOUNTS
         total, cart = process_discounts(cart, total)
-            
-        
         # ! PROCESS REST
-
-        for item in items_order:
-            amount = cart[item]
-            while True:
-                    
-
-                # If discount is appliable
-                el
-
-                else:
-                    total += amount * products_prices[item]
-                    break
 
     except Exception as e:
         total = -1
@@ -83,6 +81,7 @@ def checkout(skus):
 
 
 print(checkout("EEB"))
+
 
 
 
