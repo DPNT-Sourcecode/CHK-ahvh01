b5fd740d3ff80a4c4ce2205c23a7ed44cf190492
sbx_user1051 <sbx_user1051@169.254.36.26>
2025-01-18 06:52:54 +0000

    Sat Jan 18 06:52:54 UTC 2025

--- tes.py
+++ tes.py
@@ -14,38 +14,52 @@ def checkout(skus):
         "A": [[5, 200], [3, 130]],
         "B": [[2, 45]],
     }
-    freebies_offers = {
-        # Product: Amount, Freebie_Product, Freebie_Amount
-        "E": [2, "B", 1],
-    }
+    
 
     processed_dict = defaultdict(lambda: 0)
     total = 0
 
+    def process_freebies(cart):
+        freebies_offers = {
+            # Product: Amount, Freebie_Product, Freebie_Amount
+            "E": [2, "B", 1],
+        }
+        
+        for item, offer in freebies_offers.items():
+            if cart.get(item):
+                item_amount_required, freebie_product, freebies_amount = offer
+                if (
+                    amount // item_amount_required
+                    and cart[freebie_product] // freebies_amount
+                ):
+                    total += amount * cart[item]
+                    cart[freebie_product] -= freebies_amount
+                else:
+                    break
+                
+        
+        
+        
+
     try:
         for i in skus:
             processed_dict[i] += 1
+            
 
         # ? Workaround: Always start with E products_prices first
         items_order = list(processed_dict.keys())
         items_order.sort(reverse=True)
+        
+        # ! PROCESS FREEBIES
+        # ! PROCESS DISCOUNTS
+        # ! PROCESS REST
 
         for item in items_order:
             amount = processed_dict[item]
             while True:
                 # If feebie is valid
                 if item in freebies_offers.keys():
-                    item_amount_required = freebies_offers[item][0]
-                    freebie_product = freebies_offers[item][1]
-                    freebies_amount = freebies_offers[item][2]
-                    if (
-                        amount // item_amount_required
-                        and processed_dict[freebie_product] // freebies_amount
-                    ):
-                        total += amount * products_prices[item]
-                        processed_dict[freebie_product] -= freebies_amount
-                    else:
-                        break
+                    
 
                 # If discount is appliable
                 elif item in discount_offers.keys():
@@ -71,6 +85,7 @@ def checkout(skus):
 
 
 print(checkout("EEB"))
+
 
 
 
