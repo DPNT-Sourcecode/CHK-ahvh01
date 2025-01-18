8efef078a07ff7bf26ca1b2d9e41b8e53fbe8cec
sbx_user1051 <sbx_user1051@169.254.36.26>
2025-01-18 05:59:17 +0000

    Sat Jan 18 05:59:17 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -13,18 +13,25 @@ def checkout(skus):
         "C": 20,
         "D": 15,
     }
-    
+    offers = {
+        # Item: [Amount, Total]
+        "A": [3, 130],
+        "B": [2, 45],   
+    }
     processed_dict = defaultdict(0)
     total = 0
     try:
         for i in skus:
             processed_dict[i] += 1
 
-        processed_dict
+        for item, amount in processed_dict.items():
+            if item in offers.keys():
+                amount % offers
 
     except Exception:
         total = -1
 
     return total
+
 
 
